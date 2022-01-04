41618a237578795978560525131f46159cd75d78
Brad Kuhne <bjkuhne@aol.com>
2022-01-03 20:35:00 -0600

    could not get movie snippet to work.  Instead using Utell API to get movie venue.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var imdbButtonEl = document.querySelector("#imdbid");
-var movieSearchButtonEl = document.querySelector("#moviesearchid");
+var imdbButtonEl = document.querySelector("#getActorId");
+var movieSearchButtonEl = document.querySelector("getVenue");
 
 
 
@@ -12,7 +12,7 @@ async function getActorId() {
 	// Get the first actor's id by last name that was entered into the last name search bar
 	
 	//hard coded last name needs to be removed
-	var actorLastName = "Pacino"
+	var actorLastName = "Pitt"
 
 	await fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/"+ actorLastName + "/", {
 		"method": "GET",
@@ -24,7 +24,7 @@ async function getActorId() {
 	.then(function(response){
 		response.json().then(function(data) {
 		console.log("this is data.results in first fetch: " + data.results);
-		console.log("This should be Pacinos id: " + data.results[0].imdb_id);
+		console.log("This should be the actors id: " + data.results[0].imdb_id);
 		var actorId = data.results[0].imdb_id;
 		getMovies(actorId);
 		});
@@ -46,8 +46,36 @@ async function getMovies (actorId) {
 	})
 	.then(response => {
 		response.json().then(function(data) {
-			console.log("This is the data results of the 2nd fetch" + data);
+			console.log("This is the length of data.results: " + data.results.length);
+			for (var i = 0; i < 1; i++) { //data.results.length; i++) {
+			console.log ("This is each movie listed: " + data.results[i][0].title);
+			var movieTitle = data.results[i][0].title //*** THIS IS JUST PLACEHOLDER TO TEST WITH.  REAL CODE Should display each on screen as clickable text. */
+				
+			getMovieVenue(movieTitle);
+			}
 			console.log("This should be the title of the first movie: " + data.results[0][0].title);
+		});
+	})
+	.catch(err => {
+		console.error(err);
+	});
+};
+async function getMovieVenue (movieTitle) {
+	console.log ("About to fetch movie venue data");
+	await fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + movieTitle + "&country=us", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+			"x-rapidapi-key": "5fc5aa35cemshe0675fd52f77da4p17df4bjsn02c652d60788"
+		}
+	})
+	.then(response => {
+		response.json().then(function(data) {
+		console.log(response);
+		console.log("This is the number of venues: " + data.results.length);
+			for (var i = 0; i < data.results.length; i++) {
+				console.log ("This is a venue that the movie can be seen now: " + data.results[i].locations[0].display_name);
+			}
 		});
 	})
 	.catch(err => {
@@ -59,6 +87,7 @@ async function getMovies (actorId) {
 
 
 
+
 // add event listeners to form and button container
 imdbButtonEl.addEventListener("click", getActorId);
-//movieSearchButtonEl.addEventListener("click", movieSearchApicall);
+//movieSearchButtonEl.addEventListener("click", getVenue);
