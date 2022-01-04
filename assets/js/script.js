925ad3dd01a547d96ff0d7a890c0335a31238e95
Brad Kuhne <bjkuhne@aol.com>
2022-01-03 21:39:45 -0600

    Removed consoled log and documents how to use API code

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var movieListEl = document.querySelector("#movieList");
 async function getActorId() {
 	// Get the first actor's id by last name that was entered into the last name search bar
 	
-	//hard coded last name needs to be removed
+	//THE CODE BELOW NEEDS TO BE REPLACED WITH A VARIABLE FROM SEARCH BAR.  TEXT FROM SEARCH BAR SHOULD BE SAVED TO actorsLastName on click.
 	var actorLastName = "Wayne"
 
 	await fetch("https://data-imdb1.p.rapidapi.com/actor/imdb_id_byName/"+ actorLastName + "/", {
@@ -18,8 +18,8 @@ async function getActorId() {
 	})
 	.then(function(response){
 		response.json().then(function(data) {
-		console.log("this is data.results in first fetch: " + data.results);
-		console.log("This should be the actors id: " + data.results[0].imdb_id);
+		// console.log("this is data.results in first fetch: " + data.results);
+		// console.log("This should be the actors id: " + data.results[0].imdb_id);
 		var actorId = data.results[0].imdb_id;
 		getMovies(actorId);
 		});
@@ -41,18 +41,15 @@ async function getMovies (actorId) {
 	})
 	.then(response => {
 		response.json().then(function(data) {
-			console.log("This is the length of data.results: " + data.results.length);
+			// console.log("This is the length of data.results: " + data.results.length);
 			for (var i = 0; i < data.results.length; i++) {
-			console.log ("This is each movie listed: " + data.results[i][0].title);
-			var movieTitle = data.results[i][0].title
+				console.log ("This is each movie listed: " + data.results[i][0].title);
+				var movieTitle = data.results[i][0].title
 				
 			// display each movie  ***THESE WILL NEED TO BE CHANGED INTO LINKS TO FIND UTELLY API with getMovieVenue(movieTitle) ***
-			var movieEl = document.createElement("li");
-			movieEl.textContent = movieTitle;
-			movieListEl.appendChild(movieEl);
-
-			
-			
+				var movieEl = document.createElement("li");
+				movieEl.textContent = movieTitle;
+				movieListEl.appendChild(movieEl);
 			// getMovieVenue(movieTitle);  THIS WAS USED FOR TESTING.  THIS CALL NEEDS TO BE REPLACED WITH CALL BASED ON WHICH MOVIE IS CLICKED.
 			}
 			console.log("This should be the title of the first movie: " + data.results[0][0].title);
